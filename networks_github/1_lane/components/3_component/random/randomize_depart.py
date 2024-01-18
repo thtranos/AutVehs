6762e257f394233265e9009e7ff636e5088f754b
thtranos <157010983+thtranos@users.noreply.github.com>
2024-01-18 12:21:50 +0200

    Add files via upload

---
+++ networks_github/1_lane/components/3_component/random/randomize_depart.py
@@ -0,0 +1,22 @@
+import xml.etree.ElementTree as ET
+import random
+import numpy as np
+
+filename = '3.rou.xml'
+xmlTree = ET.parse(filename)
+rootElement = xmlTree.getroot()
+
+time = 0
+
+for type_tag in rootElement.findall('vehicle'):
+	random_number_speed = float(random.choice(np.arange(10,20)))
+	random_number_depart = float(random.choice([1,2,3,4,5,6,7]))
+	random_number_pos = float(random.choice(np.arange(0,20)))
+
+
+	type_tag.set('departSpeed', str(random_number_speed))
+	type_tag.set('departPos', str(random_number_pos))
+	type_tag.set('depart', str(time))
+	time = time + random_number_depart
+
+xmlTree.write(filename,encoding='UTF-8',xml_declaration=True)
